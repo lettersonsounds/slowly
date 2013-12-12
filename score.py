05e0a3749c98b5e565d2118156722a64bf3100b2
Erik Schoster <erik@heythere.org>
2013-12-12 17:26:22 -0600

    part two

--- score.py
+++ score.py
@@ -8,9 +8,9 @@ dsp.timer('start')
 def that(li):
     print 'layer', li
 
-    freqs = tune.fromdegrees([ dsp.randint(1, 16) for f in range(dsp.randint(3, 6)) ], octave = 0)
+    freqs = tune.fromdegrees([ dsp.randint(1, 20) for f in range(dsp.randint(3, 6)) ], octave = 3)
 
-    length = dsp.stf(dsp.rand(20, 30))
+    length = dsp.stf(dsp.rand(10, 20))
 
     layers = []
 
@@ -23,22 +23,20 @@ def that(li):
         window = dsp.randchoose(['sine', 'tri', 'hann'])
         window = dsp.wavetable(window, 512)
 
-        pulsewidth = dsp.rand(0.5, 1)
+        pulsewidth = dsp.rand(0.1, 1)
         modFreq    = dsp.rand(0.1, 10.0) / dsp.fts(length)
 
         numgrains = length / 32
-        numpoints = dsp.randint(3, 5)
+        numpoints = dsp.randint(2, 6)
 
         apoints = [0] + [dsp.rand(0, 1) for p in range(numpoints - 2)] + [0]
-        pwpoints = [dsp.rand(0.001, 1) for p in range(numpoints)]
         ppoints = [dsp.rand(0, 1) for p in range(numpoints)]
 
         e = dsp.breakpoint(apoints, numgrains)
-        pw = dsp.breakpoint(pwpoints, numgrains)
         p = dsp.breakpoint(ppoints, numgrains)
 
         mod = dsp.wavetable('random', 512)
-        modRange = dsp.rand(0, 0.005)
+        modRange = dsp.rand(0, 0.001)
 
         print '    ', round(freq, 2), round(modRange, 3), round(modFreq, 3)
         layer = dsp.pulsar(freq, length, pulsewidth, waveform, window, mod, modRange, modFreq, 0.1)
@@ -58,8 +56,8 @@ def that(li):
 
 layers = []
 
-for l in range(6):
-    layer = ''.join([ that(w) for w in range(60) ])
+for l in range(3):
+    layer = ''.join([ that(w) for w in range(40) ])
 
     layers += [ layer ]
 
